40298fa4d269249234778a8850783f22a52aff99
Aramdhan31 <160909844+Aramdhan31@users.noreply.github.com>
2024-10-19 18:54:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    const burgerMenu = document.getElementById('burgerMenu');
+    const navLinks = document.getElementById('navLinks');
+
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        navLinks.classList.toggle('show');
+    });
